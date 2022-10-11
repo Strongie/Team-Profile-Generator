61c3a31ee99a480ade897841f26c08e398318b10
Strongie <nickstrong665@gmail.com>
2022-10-12 09:35:14 +1100

    commit at any changes

--- Tests/Engineer.test.js
+++ Tests/Engineer.test.js
@@ -1,1 +1,63 @@
 const Engineer = require("./lib/Engineer.js");
+
+describe('Engineer', () => {
+    it("should return the employee name", () => {
+    
+    // Arrange
+    const name = 'John';
+    const id = '2'
+    const email = 'john@email.com'
+    const role = 'Manager'
+  
+    // Act
+    const checkEmployee = new Employee(name, id, email);
+  
+    // Assert
+    expect(checkEmployee.getName()).toEqual(name);
+      });
+  
+     
+    it("should provide the employee id when requested ", () => {
+        // Arrange
+    const name = 'John';
+    const id = '2'
+    const email = 'john@email.com'
+    const role = 'Manager'
+  
+    // Act
+    const checkEmployee = new Employee(name, id, email);
+  
+    // Assert
+    expect(checkEmployee.getID()).toEqual(id);
+      });
+
+    it("should return the employee email when requested", () => {
+    
+        // Arrange
+        const name = 'John';
+        const id = '2'
+        const email = 'john@email.com'
+        const role = 'Manager'
+      
+        // Act
+        const checkEmployee = new Employee(name, id, email);
+      
+        // Assert
+        expect(checkEmployee.getEmail()).toEqual(email);
+          });
+      
+         
+        it("should provide the employee role when requested ", () => {
+            // Arrange
+        const name = 'John';
+        const id = '2'
+        const email = 'john@email.com'
+        const role = 'Manager'
+      
+        // Act
+        const checkEmployee = new Employee(name, id, email);
+      
+        // Assert
+        expect(checkEmployee.getRole()).toEqual(id);
+          });  
+      });

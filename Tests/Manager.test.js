71550517075de61421c04e2716366fe9b336d718
Strongie <nickstrong665@gmail.com>
2022-10-12 10:57:17 +1100

    set up the tests for the manager

--- Tests/Manager.test.js
+++ Tests/Manager.test.js
@@ -1,1 +1,82 @@
 const Manager = require("./lib/Manager.js");
+
+describe('Manager', () => {
+    it("should return the manager name", () => {
+    
+    // Arrange
+    const name = 'John';
+    const id = '2'
+    const email = 'john@email.com'
+    const role = 'Manager'
+    const officeNumber = 'officenumber'
+  
+    // Act
+    const checkManager = new Manager(name, id, email, officeNumber);
+  
+    // Assert
+    expect(checkManager.getName()).toEqual(name);
+      });
+  
+     
+    it("should provide the Manager id when requested ", () => {
+        // Arrange
+        const name = 'John';
+        const id = '2'
+        const email = 'john@email.com'
+        const role = 'Manager'
+        const officeNumber = 'officenumber'
+  
+    // Act
+    const checkManager = new Manager(name, id, email, officeNumber);
+  
+    // Assert
+    expect(checkManager.getID()).toEqual(id);
+      });
+
+    it("should return the manager email when requested", () => {
+    
+        // Arrange
+        const name = 'John';
+        const id = '2'
+        const email = 'john@email.com'
+        const role = 'Manager'
+        const officeNumber = 'officenumber'
+      
+        // Act
+        const checkManager = new Manager(name, id, email, officeNumber);
+      
+        // Assert
+        expect(checkManager.getEmail()).toEqual(email);
+          });
+      
+         
+        it("should provide the manager role when requested ", () => {
+            // Arrange
+            const name = 'John';
+            const id = '2'
+            const email = 'john@email.com'
+            const role = 'Manager'
+            const officeNumber = 'officenumber'
+      
+        // Act
+        const checkManager = new Manager(name, id, email, officeNumber);
+      
+        // Assert
+        expect(checkManager.getRole()).toEqual(role);
+          });
+          
+          it("should provide the manager office number when requested ", () => {
+            // Arrange
+            const name = 'John';
+            const id = '2'
+            const email = 'john@email.com'
+            const role = 'Manager'
+            const officeNumber = 'officenumber'
+      
+        // Act
+        const checkManager = new Manager(name, id, email, officeNumber);
+      
+        // Assert
+        expect(checkManager.officeNumber).toEqual(officeNumber);
+          });    
+      });

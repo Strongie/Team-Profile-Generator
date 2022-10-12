2e9bc910a6c08bfefdcff17a088fcbebf9d9b724
Strongie <nickstrong665@gmail.com>
2022-10-12 21:59:54 +1100

    start on the index.js

--- index.js
+++ index.js
@@ -1,31 +1,63 @@
 // required packages
 const inquirer = require("inquirer");
 const fs = require("fs");
+const Manager = require("./lib/Manager.js");
+const Manager = require("./lib/Intern.js");
+const Manager = require("./lib/Employee.js");
+const Manager = require("./lib/Engineer.js");
 
 inquirer
   .prompt([
     {
       type: 'input',
       name: 'name',
-      message: 'What is your name?',
+      message: 'What is your team managers name?',
     },
     {
-      type: 'checkbox',
-      message: 'What languages do you know?',
-      name: 'stack',
-      choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+      type: 'input',
+      message: 'What is the id?',
+      name: 'id',
     },
     {
-      type: 'list',
-      message: 'What is your preferred method of communication?',
-      name: 'contact',
-      choices: ['email', 'phone', 'telekinesis'],
+      type: 'input',
+      message: 'What is your email address?',
+      name: 'email',
     },
   ])
   .then((data) => {
     const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
-
-    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
-      err ? console.log(err) : console.log('Success!')
+    const html = generateHTML(data);
+    fs.writeFile("index.html", html, (err) =>
+      err ? console.log(err) : console.log('Success !')
     );
   });
+
+  function generateHTML(data){
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+    
+        <div>
+            <ul>
+                <li>${data.name}</li>
+                <li>${data.home}</li>
+                <li>${data.hobby}</li>
+                <li>${data.food}</li>
+                <li>${data.githubname}</li>
+                <li>${data.url}</li>
+            </ul>
+           
+        </div>
+    
+    
+        
+    </body>
+    </html>
+`
+  };
